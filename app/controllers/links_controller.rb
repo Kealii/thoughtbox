b53e58d7e881074cfbe911426afb609911668811
Michael Wong <michael.brian.wong@gmail.com>
2016-02-29 12:45:49 -0700

    Adds form for new links and error checking for validations

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,24 @@
 class LinksController < ApplicationController
   before_filter :authorize
 
+  def create
+    link = current_user.links.build(link_params)
+    if link.save
+      flash[:notice] = 'Link Saved'
+      redirect_to links_path
+    else
+      flash[:notice] = 'Did not Save'
+      redirect_to links_path
+    end
+  end
+
   def index
   end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
+
 end
